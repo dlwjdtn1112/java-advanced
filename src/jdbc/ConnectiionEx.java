d0678be30adcaa066202bd45bf59da211de49dbe
dlwjdtn1112 <dlwjdtn0103@naver.com>
2025-03-04 22:26:47 +0900

    dfdf

---
+++ src/jdbc/ConnectiionEx.java
@@ -0,0 +1,35 @@
+package jdbc;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectiionEx {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection connection  =  null;
+
+
+        // 1. JDBC 드라이버 등록 : mysql db 접근 하기 위한 드라이버 등록
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("Driver loaded ok! " + connection);
+
+        // 2. Mysql DB에 연결객체를 얻어와서 연결하기
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mypra5?serverTimezone = Asia/Seoul","mypra5","mypra5");
+        System.out.println("Connention " + connection);
+
+        if(connection != null){
+            try{
+                connection.close();
+                System.out.println("conection closed");
+            }catch (SQLException e){e.printStackTrace();}
+
+        }
+
+
+
+
+    }
+
+
+}
